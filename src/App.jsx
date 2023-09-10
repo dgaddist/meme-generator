b5a53e5308815e10c91db11169401cc769a04349
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-09-09 19:19:33 -0700

    Create meme generator app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./components/Header";
+
+/**
+ * Challenge: Build the Header component
+ */
+export default function App() {
+  return <Header />;
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <div className="header">
+        <img className="troll-img" src="./Troll Face.png" />
+        <span className="Meme-title">Meme Generator</span>
+        <span className="react-title">React Course - Project 3 </span>
+      </div>
+    </div>
+  );
+}
