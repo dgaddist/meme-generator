ecc879b2d01cb3789e19024d68e8eaa743bc78b9
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-09-10 21:48:49 -0700

    Allowing random image from data once button is clicked

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,23 @@
 import memesData from "../memeData.jsx";
 
 export default function Meme() {
+  function memeButtonClick() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <div>
       <main>
-        <form className="text-search">
+        <div className="text-search">
           <input type="text" placeholder="Shut up" id="search-bar" />
           <input type="text" placeholder="and take my money" id="search-bar2" />
-          <button className="meme-button">Get a new meme image 🖼️</button>
-        </form>
+          <button className="meme-button" onClick={memeButtonClick}>
+            Get a new meme image 🖼️
+          </button>
+        </div>
       </main>
     </div>
   );
