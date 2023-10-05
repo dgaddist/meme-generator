68da456bcf22fde721d58d976ef8486ba4ce39dc
dgaddist <128024246+dgaddist@users.noreply.github.com>
2023-10-05 01:15:11 -0700

    add className for form input edit get Meme Image function to pull random image url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,19 +2,38 @@ import React from "react";
 import memesData from "../memeData.jsx";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("./memeimg.png");
+  const [memeImage, setMemeImage] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "./memeimg.png",
+  });
 
   function memeButtonClick() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMemeImage((prevMemeImage) => ({
+      ...prevMemeImage,
+      randomImage: url,
+    }));
   }
+
   return (
     <div>
       <main>
         <div className="text-search">
-          <input type="text" placeholder="Shut up" id="search-bar" />
-          <input type="text" placeholder="and take my money" id="search-bar2" />
+          <input
+            type="text"
+            placeholder="Shut up"
+            className="form--input"
+            id="search-bar"
+          />
+          <input
+            type="text"
+            placeholder="and take my money"
+            className="form--input"
+            id="search-bar2"
+          />
           <button className="meme-button" onClick={memeButtonClick}>
             Get a new meme image 🖼️
           </button>
